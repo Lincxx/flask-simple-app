578de869c62e9615c070400de55c9981f7e74b77
Jeremy Lincoln <bossco28@gmail.com>
2018-02-28 06:44:05 -0500

    first commit

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam placeat praesentium dignissimos non minus delectusid sit nihil dolor! Blanditiis vero illo est dolore laboriosam eligendi voluptate. Sunt, architecto ipsa',
+            'author': 'Jeremy',
+            'create_date':'02-27-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam placeat praesentium dignissimos non minus delectusid sit nihil dolor! Blanditiis vero illo est dolore laboriosam eligendi voluptate. Sunt, architecto ipsa',
+            'author': 'Brad',
+            'create_date':'02-27-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam placeat praesentium dignissimos non minus delectusid sit nihil dolor! Blanditiis vero illo est dolore laboriosam eligendi voluptate. Sunt, architecto ipsa',
+            'author': 'Jeremy',
+            'create_date':'02-27-2018'
+        }
+    ]
+
+    return articles
